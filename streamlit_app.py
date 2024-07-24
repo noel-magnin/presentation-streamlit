9c286428c253da9f46312bb0c350e17a14a85340
noel-magnin <41486605+noel-magnin@users.noreply.github.com>
2024-07-24 16:52:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 st.title('🎈 Presentation App')
-components.iframe("https://www.canva.com/design/DAGL3lqD0iY/zDA4vmHCFNClJckRcLCSiA/view", height=512)
+components.iframe("https://www.canva.com/design/DAGL3lqD0iY/4rI3RGYxPBXUrUUczq7sbw/edit?utm_content=DAGL3lqD0iY&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton", height=512)
